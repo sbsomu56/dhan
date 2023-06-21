d601370e2865b0933eebf827c612e4d83c60fa56
Replit user <>
2023-06-21 11:02:56 +0000

    start adding 15ema screener

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
 import pandas as pd
 from dhanhq import dhanhq
-# import talib
+from tqdm import tqdm
+import time
+import talib
 
 # Create DHANHQ object for sourcing data:
 client_id = os.getenv('DHAN_CLIENT_ID')
@@ -11,6 +13,8 @@ dhan = dhanhq(client_id, access_token)
 
 # load api-script-master
 script_master = pd.read_csv('data/api-scrip-master.csv')
+nifty50 = pd.read_csv('data/NIFTY50.csv')
+niftynext50 = pd.read_csv('data/NIFTYNEXT50.csv')
 
 
 # FUNCTIONS:
@@ -85,6 +89,7 @@ def home():
 
 @app.route('/positions')
 def positions():
+  # return render_template('positions.html')
   positions = dhan.get_positions()['data']
   df = pd.DataFrame(positions)
 
@@ -112,9 +117,67 @@ def positions():
                          titles=df.columns.values)
 
 
+@app.route('/get-positions')
+def get_positions():
+  positions = dhan.get_positions()['data']
+  df = pd.DataFrame(positions)
+
+  df['LTP'] = df['securityId'].map(lambda x: get_ltp(x))
+
+  # df['PnL'] = (df['LTP'] - df['buyAvg']) * df['buyQty']
+
+  idx_long = df['positionType'] == 'LONG'
+  df.loc[idx_long,
+         'PnL'] = (df.loc[idx_long, 'LTP'] * df.loc[idx_long, 'buyQty'] -
+                   df.loc[idx_long, 'dayBuyValue'])
+
+  idx_short = df['positionType'] == 'SHORT'
+  df.loc[idx_short,
+         'PnL'] = (-df.loc[idx_short, 'LTP'] * df.loc[idx_short, 'sellQty'] +
+                   df.loc[idx_short, 'sellAvg'])
+
+  idx_closed = df['positionType'] == 'CLOSED'
+  df.loc[idx_closed, 'PnL'] = (-df.loc[idx_closed, 'dayBuyValue'] +
+                               df.loc[idx_closed, 'daySellValue'])
+
+  df = df[position_col].to_json()
+
+  return jsonify(df)
+
+
 @app.route('/screener-15-ema')
 def screener_15_ema():
-  return render_template('screener_15_ema.html')
+  date = "2023-06-09"
+  all_stocks = pd.concat([nifty50, niftynext50], ignore_index=True)
+  stock_list = all_stocks.loc[1:, 'Symbol']
+
+  # df to store entry and stop-loss:
+  data = []
+
+  for indicator in tqdm(stock_list):
+    time.sleep(0.2)
+    # print(indicator)
+    if indicator in ['NIFTY 50', 'NIFTY NEXT 50']:
+      continue
+    try:
+      df = dhan.historical_minute_charts(indicator, 'NSE_EQ', 'EQUITY', 0,
+                                         '2022-01-01', date)['data']
+      df = ConvertDailyToWeekly(df)
+      t0 = df.iloc[-1]
+      if (t0['ema'] > t0['high']):
+        data.append({
+          "TICKER": indicator,
+          "ENTRY": t0['high'],
+          "STOP_LOSS": t0['low']
+        })
+        #print("5 EMA triggered for stock: ",indicator)
+    except:
+      print('Error in data fetching for ', indicator)
+
+  data = pd.DataFrame(data)
+  return render_template('screener_15_ema.html',
+                         tables=[data.to_html(classes='data')],
+                         titles=data.columns.values)
 
 
 if __name__ == "__main__":
